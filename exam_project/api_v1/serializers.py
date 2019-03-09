ff09615dbb3fbeda01f15959eb2a34768c8a1a19
armani <razzarioa@gmail.com>
2019-03-09 12:11:16 +0600

    Wrote TaskSerializer and viewsets in views.py

---
+++ exam_project/api_v1/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from webapp.models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:task-detail')
+
+    class Meta:
+        model = Task
+        fields = ('summary', 'url', 'description', 'due_date', 'status', 'time_planned')

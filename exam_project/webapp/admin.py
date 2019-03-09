d1af55af1dc91d39b87e692c9fb2486144398e00
armani <razzarioa@gmail.com>
2019-03-09 11:44:01 +0600

    Migrated model, registered in admin.py

--- exam_project/webapp/admin.py
+++ exam_project/webapp/admin.py
@@ -1,2 +1,11 @@
 from django.contrib import admin
 from webapp.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'summary', 'status']
+    ordering = ['-status']
+    search_fields = ['summary', 'id']
+
+
+admin.site.register(Task, TaskAdmin)

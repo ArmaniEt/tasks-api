5ca4949b97445a2d19e8e49f8fd7ea5d57914f6a
armani <razzarioa@gmail.com>
2019-03-09 11:38:06 +0600

    Created model Task, added apps in settings.py, imported Task in admin.py

--- exam_project/webapp/models.py
+++ exam_project/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    TASK_STATUS = (
+        ('Queue', 'В очереди'),
+        ('In progress', 'В работе'),
+        ('Done', 'Сделано')
+    )
+
+    summary = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, blank=True, null=True)
+    due_date = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=TASK_STATUS, default='Queue')
+    time_planned = models.DecimalField(max_digits=10, decimal_places=1, blank=True, null=True)
